a1e8e14f1e50d8120300c73a4945bfa63bd90ff2
gppandey123 <gppandey9919@gmail.com>
2020-12-25 19:34:25 +0530

    Updates

---
+++ precache-manifest.f96307a4e32210957d70230ddc3e0e72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b001cccdba99483810a4347b8ca406e",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "0b282e2d4c01a68e4a30",
+    "url": "/movie-search/static/css/main.0772324f.chunk.css"
+  },
+  {
+    "revision": "574f0275a0c449c8a6a9",
+    "url": "/movie-search/static/js/2.8fbe69bd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie-search/static/js/2.8fbe69bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b282e2d4c01a68e4a30",
+    "url": "/movie-search/static/js/main.b8a5c0ad.chunk.js"
+  },
+  {
+    "revision": "8dfebb08ca09d0c6f6c1",
+    "url": "/movie-search/static/js/runtime-main.17d9c4f9.js"
+  }
+]);
